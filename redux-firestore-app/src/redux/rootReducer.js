8ac2b412fe8cc039e0e17f83a3d8648e8dbf36c1
MalshaNavodi <malsha1310weerasinghe@gmail.com>
2021-10-15 23:17:15 +0530

    Contact-initial-commit
    Add Contact
    Delete Contact
    View Contact

--- redux-firestore-app/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import contactReducer from "./reducer";
-
-const rootReducer = combineReducers({
-    data: contactReducer,
-});
-
-export default rootReducer;
